5a69e1175fb1d6e043796bb6b44a9bff1fb47122
Shobanasri G <shobanasrisrmcse@gmail.com>
2026-04-23 18:52:24 +0530

    Create QuantityMeasurementApp1.java

---
+++ QuantityMeasurementApp1.java
@@ -0,0 +1,19 @@
+package com.apps.quantitymeasurement;
+public class QuantityMeasurementApp{
+  public static class Feet{
+    private final double value;
+    public feet(double value){
+      this.value = value;
+    }
+    @Override
+    public boolean equals(Object obj){
+      if (this == obj){
+        return true;
+      }
+      if(obj == null || getClass() != obj.getClass()){
+        return false;
+      }
+      Feet feet = (Feet) obj;
+      return Double.compare(this.value, feet.value) == 0;
+    }
+  }
